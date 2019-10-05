1c300096a07d3c261cc65875b10a17a490e4503c
Mohit <mkvmb.6@gmail.com>
2019-10-05 14:11:12 +0530

    Add project files.

---
+++ MyAspNetCore3Test/Controllers/WeatherForecastController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MyAspNetCore3Test.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] mySummaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> myLogger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            myLogger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = mySummaries[rng.Next(mySummaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
